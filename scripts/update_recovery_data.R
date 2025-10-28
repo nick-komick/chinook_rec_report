2acfd0993f549301b4e30415f71129f785aa6e65
Nicholas Komick <nicholas.komick@dfo-mpo.gc.ca>
2025-10-28 11:04:18 -0700

    further edits and add irec data

--- scripts/update_recovery_data.R
+++ scripts/update_recovery_data.R
@@ -20,14 +20,14 @@ cmd_params <- c("get", "psc", "CDFO")
 Sys.setenv(MRP_YAML_ENVIRONMENT = "production")
 
 
-#pw <- askpass::askpass("CWT Database password")
+pw <- askpass::askpass("CWT Database password")
 
-#if (file.exists(cwt_report_dir) == FALSE){
-#  dir.create(cwt_report_dir)
-#}
-#mrpAdminMain(c(cmd_params, "2005:2009"), mrp_config_filename, db_password = pw)
-#mrpAdminMain(c(cmd_params, "2010:2019"), mrp_config_filename, db_password = pw)
-#mrpAdminMain(c(cmd_params, "2020:2024"), mrp_config_filename, db_password = pw)
+if (file.exists(cwt_report_dir) == FALSE){
+  dir.create(cwt_report_dir)
+}
+mrpAdminMain(c(cmd_params, "2005:2009"), mrp_config_filename, db_password = pw)
+mrpAdminMain(c(cmd_params, "2010:2019"), mrp_config_filename, db_password = pw)
+mrpAdminMain(c(cmd_params, "2020:2024"), mrp_config_filename, db_password = pw)
 
 
 catch_years <- 2005:2024
@@ -35,5 +35,5 @@ report_filenames <- file.path(cwt_report_dir,paste0("RC042_CDFO_", catch_years, 
 
 cetl::campImportRecFile(camp_conn_filename = user_settings_filename,
                         rec_filenames = report_filenames,
-                        test_mode = TRUE,
+                        test_mode = FALSE,
                         update_sync_tbl = FALSE)
